ddad0736c3483db192405b126532f8de5038fd44
Monkin <monkinsmurf@gmail.com>
2023-01-03 13:17:43 +0000

    Add Simple example to receive sensor data

--- src/examples/px4_hello_world_app/px4_hello_world_app.c
+++ src/examples/px4_hello_world_app/px4_hello_world_app.c
@@ -38,12 +38,56 @@
  * @author Example User <mail@example.com>
  */
 
+#include <poll.h>
+#include <unistd.h>
+#include <stdio.h>
+
 #include <px4_platform_common/log.h>
+#include <px4_platform_common/px4_config.h>
+#include <px4_platform_common/tasks.h>
+#include <px4_platform_common/posix.h>
+
+#include <uORB/topics/sensor_combined.h>
+#include <uORB/topics/vehicle_acceleration.h>
+#include <uORB/topics/vehicle_attitude.h>
 
 __EXPORT int px4_hello_world_app_main(int argc, char *argv[]);
 
 int px4_hello_world_app_main(int argc, char *argv[])
 {
 	PX4_INFO("Hello Space!");
+
+	// Testing Sensor subscriptions
+	int sensor_sub_fd = orb_subscribe(ORB_ID(sensor_combined));
+
+	px4_pollfd_struct_t fds[] = {
+		{ .fd = sensor_sub_fd, .events = POLLIN},
+	};
+
+	while (true) {
+		/* wait for sensor update of 1 file descriptor for 1000ms (1s) */
+		int poll_ret = px4_poll(fds, 1, 1000);
+		// Temporary log to use the poll_ret variable
+		printf("poll_ret: %d\n", poll_ret);
+
+		if (fds[0].revents & POLLIN) {
+			/* obtained data for the first file descriptor */
+			struct sensor_combined_s raw;
+
+			/* copy sensors raw data into local buffer */
+			orb_copy(ORB_ID(sensor_combined), sensor_sub_fd, &raw);
+
+			// Log the sensor data
+			PX4_INFO("Accelerometer:\t%8.4f\t%8.4f\t%8.4f", 
+				(double) raw.accelerometer_m_s2[0],
+				(double) raw.accelerometer_m_s2[1],
+				(double) raw.accelerometer_m_s2[2]
+ 			);
+
+			// Stop the loop to display only once
+			break;
+		}
+	}
+
 	return OK;
 }
